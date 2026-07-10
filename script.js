bc217a45ab3fc1ac9a0b24b880ee2293fb9f9c9b
laibanawaz45 <laiba nawaz2923@gmail.com>
2026-07-10 22:41:17 +0500

    bg-changer

---
+++ script.js
@@ -0,0 +1,13 @@
+//  var show = document.getElementById('show');
+
+function red(para1){
+document.body.style.backgroundColor = para1;
+}
+function green(para2){
+document.body.style.backgroundColor = para2;
+
+}
+function blue(para3){
+document.body.style.backgroundColor = para3;
+
+}
