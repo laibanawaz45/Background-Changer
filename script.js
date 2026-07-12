00687c7a515dfea3c5707e809fd958ac2a042868
laibanawaz45 <laiba nawaz2923@gmail.com>
2026-07-12 22:24:17 +0500

    changes

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ function blue(para3){
 document.body.style.backgroundColor = para3;
 
 }
+function pink(para4){
+    document.body.style.backgroundColor =para4;
+}
+function yellow(para5){
+    document.body.style.backgroundColor = para5;
+}
+function orange(para6){
+    document.body.style.backgroundColor = para6;
+}
+function brown(para7){
+    document.body.style.backgroundColor = para7;
+}
